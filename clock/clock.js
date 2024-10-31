f25536e6dae2fd0b6424e665134d74e897209f70
Dain <ekdls5004@naver.com>
2024-10-31 15:38:12 +0900

    1031clock,timer

--- clock/clock.js
+++ clock/clock.js
@@ -2,5 +2,9 @@ const now = new Date();
 
 console.log(now)
 
-// const then = new Date(2008, 5, 10)
-// console.log(then)
+const then = new Date(1998, 12, 16)
+console.log(then)
+
+const xxx = now - then;
+console.log(xxx / (365*1000*60*60*24))
+console.log(xxx / (1000*60*60*24))
